d59a7b1e492e10b1b90b0103bc92e8c5b8a7e3f7
AdomasLiutkus1997 <Adomas.liutkus@teliacompany.com>
2023-09-07 11:38:38 +0300

    Added Tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,6 @@ module.exports = {
   plugins: ['testing-library', 'jest-dom', 'react', '@typescript-eslint'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
   },
 };
